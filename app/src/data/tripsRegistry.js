b26f43a79ecc47e896ae5b1343ab888cd677dbe2
nutjaa <nutjaa@msn.com>
2026-08-12 22:32:26 +0700

    feat: add Bali 2027 trip data and associated images to registry

--- app/src/data/tripsRegistry.js
+++ app/src/data/tripsRegistry.js
@@ -1,8 +1,10 @@
 import japan2026 from './trips/japan-2026.json';
 import hokkaido2027 from './trips/hokkaido-2027.json';
+import bali2027 from './trips/bali-2027.json';
 
 export const allTrips = [
   japan2026,
+  bali2027,
   hokkaido2027
 ];
 
